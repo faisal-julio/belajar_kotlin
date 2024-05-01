75f35fbca73e8b848952d7550d0b9cca614e12cb
MyPC <faisaljulio11.fj@gmail.com>
2024-05-01 21:57:31 +0700

    icon

--- app/src/main/java/com/example/faisal/MainActivity.kt
+++ app/src/main/java/com/example/faisal/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,16 +16,22 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,23 +58,37 @@ fun userprofile() {
             Image(
                 painter = painterResource(id = R.drawable.batu),
                 contentDescription = "batu",
-                modifier = Modifier.size(60.dp).offset(y = 15.dp,x = 20.dp)
+                modifier = Modifier
+                    .size(70.dp)
+                    .offset(y = 10.dp, x = 10.dp)
+                    .border(2.dp, Color.Red, CircleShape)
+                    .clip(CircleShape)
             )
+            Spacer(modifier = Modifier.width(40.dp))
             Column(modifier = Modifier
-                .padding(20.dp)
-                .background(Color.Transparent)) {
+                .weight(1f)
+                .padding(15.dp)
+                .background(Color.Transparent)
+                .fillMaxWidth()){
                 Text(
                     text = "Faisal Yulianto",
+                    fontWeight = FontWeight.ExtraBold,
                     fontSize = 20.sp,
                     color = Color.White
                 )
                 Text(
                     text = "312210646",
-                    fontSize = 20.sp,
+                    fontSize = 15.sp,
                     color = Color.White
                 )
             }
-
+            Icon(imageVector = Icons.Filled.AccountBox ,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(30.dp)
+            )
             }
 
 
